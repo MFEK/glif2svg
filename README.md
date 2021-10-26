3ab2732a31fcf0b6139fdc8156d6ec3ed17131ce
Fredrick Brennan <copypaste@kittens.ph>
2021-10-25 20:29:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Convert UFO glyph files (`.glif`) to SVG
+
+There exists already an `svg2glif`, but for some reason not the opposite operation. My MFEKglif editor treats `.glif` files as first-class entities, so it seemed obvious to me I'd want to edit them with Inkscape, etc sometimes.
+
+I originally wrote this in Python ([ctrlcctrlv/glif2svg](https://github.com/ctrlcctrlv/glif2svg)) but it was too slow for the huge amount I was using it in FRBAmericanCursive's build. So, I ported it to Rust. Also, I decided to make it part of the MFEK suite of programs, since it heavily relies on MFEK libaries like glifparser and mfek-ipc.
+
+```
+glif2svg 0.0.0
+Fredrick R. Brennan <copypasteⒶkittens⊙ph>; MFEK Authors
+Convert between glif to SVG
+
+USAGE:
+    glif2svg [FLAGS] [OPTIONS] <input>
+
+FLAGS:
+    -h, --help            Prints help information
+    -M, --no-metrics      Don't consider glif's height/width when writing SVG, use minx/maxx/miny/maxy
+    -T, --no-transform    Don't put transform="translate(…)" in SVG
+    -V, --version         Prints version information
+
+OPTIONS:
+    -o, --output <output_file>    The path to the output file. If not provided, or `-`, stdout.
+                                  
+                                  
+    -F, --fontinfo <fontinfo>     fontinfo file (for metrics, should point to fontinfo.plist path)
+                                  
+
+ARGS:
+    <input>    The path to the input file.
+```
+
+## Requirements
+
+This is a normal Rust build; however the final binary will expect you to have [MFEKmetadata](https://github.com/MFEK/metadata) in your path as well.
+
+## License
+
+```
+Copyright 2021 Fredrick R. Brennan & MFEK Authors
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this software or any of the provided source code files except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations under the License.
+```
+
+**By contributing you release your contribution under the terms of the license.**
---
+++ src/main.rs
@@ -0,0 +1,195 @@
+use glifparser;
+use clap::{self, App, AppSettings, Arg};
+use skia_safe::{Point, path::Verb};
+use skia_safe::path::Iter as SkIter;
+use mfek_ipc::{self, Available, IPCInfo};
+use xmltree;
+
+use std::{fs, str as stdstr};
+
+struct SVGPathPen {
+    path: String,
+    minx: f64,
+    maxx: f64,
+    miny: f64,
+    maxy: f64
+}
+
+fn consider_min_max(svg: &mut SVGPathPen, points: &[Point]) {
+    for p in points {
+        if (svg.minx as f32) > p.x { svg.minx = p.x.into(); }
+        if (svg.maxx as f32) < p.x { svg.maxx = p.x.into(); }
+        if (svg.miny as f32) > p.y { svg.miny = p.y.into(); }
+        if (svg.maxy as f32) < p.y { svg.maxy = p.y.into(); }
+    }
+}
+
+impl SVGPathPen {
+    fn new() -> Self {
+        Self { path: String::new(), minx: 0., miny: 0., maxx: 0., maxy: 0. }
+    }
+
+    fn extend_path(&mut self, path: &str) {
+        self.path.push_str(path);
+    }
+
+    #[allow(non_snake_case)]
+    fn viewBox(&self) -> (f64, f64, f64, f64) {
+        return (self.minx, self.miny, self.maxx+(self.minx).abs(), self.maxy+(self.miny).abs())
+    }
+
+    #[allow(non_snake_case)]
+    fn viewBox_str(&self) -> String {
+        let (x, y, dx, dy) = self.viewBox();
+        return format!("{:.0} {:.0} {:.0} {:.0}", x, y, dx, dy);
+    }
+
+    fn transform(&self) -> String {
+        return format!("translate(0 {:.0})", (-self.maxy).abs() - (self.miny).abs());
+    }
+
+    fn move_to(&mut self, pt: Point) {
+        consider_min_max(self, &[pt]);
+        self.extend_path(&format!("M {} {}", pt.x, -pt.y));
+    }
+
+    fn line_to(&mut self, pt: Point) {
+        consider_min_max(self, &[pt]);
+        self.extend_path(&format!("L {} {}", pt.x, -pt.y));
+    }
+
+    fn curve_to(&mut self, pt: &[Point]) {
+        consider_min_max(self, pt);
+        self.extend_path(&format!("C {} {} {} {} {} {}", pt[0].x, -pt[0].y, pt[1].x, -pt[1].y, pt[2].x, -pt[2].y));
+    }
+
+    fn qcurve_to(&mut self, pt: &[Point]) {
+        consider_min_max(self, pt);
+        self.extend_path(&format!("Q {} {} {} {}", pt[0].x, -pt[0].y, pt[1].x, -pt[1].y));
+    }
+
+    fn close_path(&mut self) {
+        self.extend_path("Z");
+    }
+}
+
+fn main() {
+    let matches = App::new("glif2svg")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .version("0.0.0")
+        .author("Fredrick R. Brennan <copypasteⒶkittens⊙ph>; MFEK Authors")
+        .about("Convert between glif to SVG")
+        .arg(Arg::with_name("input_file")
+            .short("in")
+            .long("input")
+            .takes_value(true)
+            .conflicts_with("input")
+            .hidden(true))
+        .arg(Arg::with_name("input")
+            .index(1)
+            .help("The path to the input file.")
+            .conflicts_with("input_file")
+            .required_unless("input_file"))
+        .arg(Arg::with_name("output_file")
+            .short("out")
+            .long("output")
+            .takes_value(true)
+            .conflicts_with("output")
+            .display_order(1)
+            .help("The path to the output file. If not provided, or `-`, stdout.\n\n\n"))
+        .arg(Arg::with_name("output")
+            .index(2)
+            .hidden(true))
+        .arg(Arg::with_name("no_transform")
+            .short("T")
+            .long("no-transform")
+            .help("Don't put transform=\"translate(…)\" in SVG"))
+        .arg(Arg::with_name("no_metrics")
+            .short("M")
+            .long("no-metrics")
+            .help("Don't consider glif's height/width when writing SVG, use minx/maxx/miny/maxy"))
+        .arg(Arg::with_name("fontinfo")
+            .short("F")
+            .long("fontinfo")
+            .takes_value(true)
+            .help("fontinfo file (for metrics, should point to fontinfo.plist path)\n\n"))
+        .get_matches();
+
+    let input = matches.value_of("input").unwrap_or_else(||matches.value_of("input_file").unwrap());
+    let output = matches.value_of("output").or_else(||matches.value_of("output_file"));
+    let no_transform = matches.is_present("no_transform");
+    let no_metrics = matches.is_present("no_metrics");
+    let fontinfo_o = matches.value_of("fontinfo");
+
+    let glif: glifparser::Glif<()> = glifparser::glif::read_from_filename(matches.value_of("input").unwrap()).unwrap();
+
+    let mut svg = SVGPathPen::new();
+
+    use glifparser::outline::skia::ToSkiaPaths;
+    let skia_paths = glif.outline.unwrap().to_skia_paths(None);
+    for path in skia_paths.open.iter().chain(skia_paths.closed.iter()) {
+        let iter = SkIter::new(&path, false);
+        for (verb, pts) in iter {
+            match verb {
+                Verb::Move => svg.move_to(pts[0]),
+                Verb::Line => svg.line_to(pts[0]),
+                Verb::Quad => svg.qcurve_to(&pts),
+                Verb::Cubic => svg.curve_to(&pts),
+                Verb::Close => svg.close_path(),
+                _ => {unimplemented!()}
+            }
+        }
+    }
+
+    if !no_metrics {
+        svg.minx = 0.;
+        svg.maxx = glif.width.unwrap_or(0) as f64;
+    }
+
+    let (status, _) = mfek_ipc::module_available("metadata".into());
+    if status == Available::Yes && !no_metrics {
+        let ipc_info = if let Some(fi) = fontinfo_o {
+            IPCInfo::from_fontinfo_path("glif2svg".to_string(), &fi)
+        } else {
+            IPCInfo::from_glif_path("glif2svg".to_string(), &input)
+        };
+
+        if let Ok((ascender, descender)) = mfek_ipc::helpers::metadata::ascender_descender(&ipc_info) {
+            svg.maxy = ascender as f64;
+            svg.miny = descender as f64;
+        } else {
+            eprintln!("Failed to set metrics of SVG from glif font!");
+        }
+    } else {
+        eprintln!("MFEKmetadata REQUIRED for sane UFO metrics into SVG");
+    }
+
+    let mut svgxml = xmltree::Element::new("svg");
+    svgxml.attributes.insert("version".to_owned(), "1.1".to_owned());
+    svgxml.attributes.insert("xmlns".to_owned(), "http://www.w3.org/2000/svg".to_owned());
+    svgxml.attributes.insert("viewBox".to_owned(), svg.viewBox_str());
+
+    let mut pathxml = xmltree::Element::new("path");
+    if !no_transform {
+        pathxml.attributes.insert("transform".to_owned(), if !no_metrics {
+            format!("translate(0 {})", svg.miny.abs())
+        } else {
+            svg.transform()
+        });
+    }
+    pathxml.attributes.insert("d".to_owned(), svg.path);
+    svgxml.children.push(xmltree::XMLNode::Element(pathxml));
+
+    let config = xmltree::EmitterConfig::new().perform_indent(true);
+    let mut outxml = Vec::<u8>::new();
+
+    svgxml.write_with_config(&mut outxml, config).unwrap();
+
+    if let Some(outfile) = output {
+        if outfile != "-" {
+            fs::write(outfile, &outxml).unwrap();
+            return
+        }   
+    }
+    print!("{}", stdstr::from_utf8(&outxml).unwrap());
+}
